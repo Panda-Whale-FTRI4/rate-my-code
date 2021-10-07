51532124a2a074ea731e9730860ce2407e616642
Parker Hutcheson <pdhutcheson@gmail.com>
2021-10-07 16:34:14 -0700

    Added basic tests for routes

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ const request = require('supertest');
 const fs = require('fs');
 const path = require('path');
 
-
 const server = 'http://localhost:8080';
 
+//Began to implement a Mocha test suite utilizing supertest to test our routes.
 
 describe('Main Route Integration', () => {
   describe('/', () => {
@@ -23,9 +23,6 @@ describe('Main Route Integration', () => {
 
   describe('/getPost', () => {
     describe('GET', () => {
-      // Note that we return the evaluation of `request` here! It evaluates to
-      // a promise, so Jest knows not to say this test passes until that
-      // promise resolves. See https://jestjs.io/docs/en/asynchronous
       it('responds with 200 status and text/html content type', () => {
         return request(server)
           .get('/')
@@ -34,4 +31,17 @@ describe('Main Route Integration', () => {
       });
     });
   });
+
+  describe('/createPost', () => {
+    describe('POST', () => {
+      it('responds with 200 status', () => {
+        return request(server)
+          .get('/')
+          .expect(200);
+      });
+    });
+  });
 });
+
+
+
