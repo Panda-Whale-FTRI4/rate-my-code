97f1382e16023d113972494942033b8950a6be92
Parker Hutcheson <pdhutcheson@gmail.com>
2021-10-07 16:11:36 -0700

    Added Test Suite File

---
+++ test/test.js
@@ -0,0 +1,37 @@
+const request = require('supertest');
+const fs = require('fs');
+const path = require('path');
+
+
+const server = 'http://localhost:8080';
+
+
+describe('Main Route Integration', () => {
+  describe('/', () => {
+    describe('GET', () => {
+      // Note that we return the evaluation of `request` here! It evaluates to
+      // a promise, so Jest knows not to say this test passes until that
+      // promise resolves. See https://jestjs.io/docs/en/asynchronous
+      it('responds with 200 status and text/html content type', () => {
+        return request(server)
+          .get('/')
+          .expect('Content-Type', /text\/html/)
+          .expect(200);
+      });
+    });
+  });
+
+  describe('/getPost', () => {
+    describe('GET', () => {
+      // Note that we return the evaluation of `request` here! It evaluates to
+      // a promise, so Jest knows not to say this test passes until that
+      // promise resolves. See https://jestjs.io/docs/en/asynchronous
+      it('responds with 200 status and text/html content type', () => {
+        return request(server)
+          .get('/')
+          .expect('Content-Type', /text\/html/)
+          .expect(200);
+      });
+    });
+  });
+});
