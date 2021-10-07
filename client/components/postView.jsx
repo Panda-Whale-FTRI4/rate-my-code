33c3c3f27681784440a0f4971e8df794bff5ba74
isaiah delgado <isaiah_del@yahoo.com>
2021-10-07 19:35:00 -0400

    made more styling changes

--- client/components/postView.jsx
+++ client/components/postView.jsx
@@ -1,4 +1,6 @@
+import { ClassNames } from '@emotion/react';
 import React, { useEffect, useState } from 'react';
+import classes from './PostView.module.css';
 
 export default function PostView (props) {
   
@@ -15,23 +17,37 @@ export default function PostView (props) {
     fetch(`/api/getPost/${props.postToRender}`)
       .then((res) => res.json())
       .then((data) => {
-        setPost(data?.post);
+        setPost(data?.posts);
         setComments(data?.comments);
       })
       .catch((err) => console.log(err));
   };
-  const array = ['hello', 'how are you']  ;
-  
 
   return (
-    <div>
-      {console.log(comments[0])}
-      {console.log(array)}
-
+    
+    <div className={classes.container}>
+      <div className={classes.post}>
+        <h2 className={classes.title}>{post?.title}</h2>
+        <div className={classes.issue}>
+          <h3>Issue:  </h3>
+          {post?.issue}
+        </div>
+        <div className={classes.code}>
+          <h3>Code:  </h3> 
+          {post?.code}
+        </div>
+        <div>{post?.username}</div>
+      </div>
+      <div className={classes.votes}>
+        <div className={classes.upvotes}>Upvotes: {post?.downvotes}</div>
+        <div className={classes.downvotes}>Downvotes: {post?.upvotes}</div>
+      </div>
+      <div className={classes.comments}> 
+        <h3 className={classes.commentTitle}>Comments</h3>
+        <div>Comment1</div>
+        <div>Comment2</div>
+      </div>
       
-      {/* <div>{post?.title}</div>
-      <div>{post?.issue}</div> */}
-      <div>{comments}</div>
     </div>
   );
 }
